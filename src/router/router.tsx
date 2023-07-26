054755608aa1832b34c0dad5476fbf67d59cbb5c
KanhaKanha123 <kanaiyakamboj23@gmail.com>
2023-07-26 23:37:38 +0200

    add error boundary to app to not crash whole app

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
+import ErrorBoundary from '../error-boundry/error-boundry';
 import { Home } from '../pages/home/home-page';
 
 export const router = createBrowserRouter([
     {
-        element: <Home />,
+        element: <ErrorBoundary><Home /></ErrorBoundary>,
         path: ''
     }
 ]);
