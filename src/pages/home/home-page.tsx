b791c8a2f9b463df989090d4279b045a3ffeed4f
KanhaKanha123 <kanaiyakamboj23@gmail.com>
2023-07-26 23:29:34 +0200

    created higher level home component to display app higher level design

---
+++ src/pages/home/home-page.tsx
@@ -0,0 +1,12 @@
+import { FC, ReactElement } from "react";
+
+/**
+ * This heigher level component is responsible render the products details lists on router request.
+ * @returns ReactElement
+ */
+export const Home: FC = (): ReactElement => {
+
+    return <main area-label="main section to display all the product detail cards">
+        Hello word
+    </main>
+};
